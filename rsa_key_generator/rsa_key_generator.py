126dc1061ba0a7ad9bc8c8d660dc42040315376a
Sayyid Masagena <kazamasensei15@gmail.com>
2022-12-03 14:42:22 +0800

    first commit

---
+++ rsa_key_generator/rsa_key_generator.py
@@ -0,0 +1,18 @@
+'''
+WELCOME TO THE RSA KEY GENERATOR. THIS IS AN INTERACTIVE TOOL USED TO ENCRYPT OR DECRYPT A MESSAGE USING THE FAMOUS RSA ALGORITHM.
+ 
+SAYYID SHIDDIQ MASAGENA (D121191014)
+A. MUH RAYYAN EKA PUTRA (D121191074)
+'''
+ 
+import math
+from pickle import FALSE
+ 
+print("RSA KEY GENERATOR")
+print("*****************************************************")
+ 
+#Input Prime Numbers
+print("PLEASE ENTER THE 'p' AND 'q' VALUES BELOW:")
+p = int(input("Enter a prime number for p: "))
+q = int(input("Enter a prime number for q: "))
+print("*****************************************************")
