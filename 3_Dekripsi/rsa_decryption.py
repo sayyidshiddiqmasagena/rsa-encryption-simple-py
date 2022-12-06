133e0666b39393861bb9f0a9ea53eb56c52f940f
Rayyan Eka Putra <bullzhit99@gmail.com>
2022-12-06 22:12:40 +0800

    memperbaiki outputnya

--- 3_Dekripsi/rsa_decryption.py
+++ 3_Dekripsi/rsa_decryption.py
@@ -1,6 +1,4 @@
 '''
-WELCOME TO THE RSA DECRYPTION. THIS IS AN INTERACTIVE TOOL USED TO DECRYPT A MESSAGE USING THE FAMOUS RSA ALGORITHM.
- 
 SAYYID SHIDDIQ MASAGENA (D121191014)
 A. MUH RAYYAN EKA PUTRA (D121191074)
 '''
@@ -8,20 +6,15 @@ A. MUH RAYYAN EKA PUTRA (D121191074)
 import math
 from pickle import FALSE
  
-print("RSA DECRYPTION")
-print("*****************************************************")
- 
-print("PLEASE ENTER THE 'd' AND 'n' VALUES BELOW:")
-d = int(input("Enter the value of private key 'd': "))
-n = int(input("Enter the value of number 'n': "))
+print("=== RSA Dekripsi ===")
 
-print("*****************************************************")
-print("PRIVATE KEY")
+print("Masukkan private key 'd' dan gembok 'n'-mu!")
+d = int(input("private key 'd': "))
+n = int(input("gembok 'n': "))
+
 private = (d,n)
-print("Private Key is:",private)
-print("*****************************************************")
  
-'''DECRYPTION ALGORITHM.'''
+#Algoritma Dekripsi
 def decrypt(priv_key,c_text):
     d,n=priv_key
     txt=c_text.split(',') #string dalam list harus diconvert jdi int
@@ -31,18 +24,17 @@ def decrypt(priv_key,c_text):
         if(int(i)== 400):
             x+=' '
         else:
-            m=(int(i)**d)%n #enkripsi^private key mod n, utk mendekripsi
+            m=(int(i)**d)%n #enkripsi^private key mod n, utk mendekripsi ada di buku
 
             m+=65 #supaya semuanya mulai dari 0
             c=chr(m) #int --> char
-            x+=c #membentuk string tiap char tanpa list
+            x+=c #membentuk string tiap char tanpa list --> karena c != [] tapi c = ''
     return x
  
-message = input("What message would you like to be decrypted?:  ")
-print("Your message is:", message)
 
+message = input("Pesan yang ingin dienkripsi (untuk saat ini hanya bisa text):  ")
+print("Pesanmu adalah:",message)
 enc_msg=decrypt(private,message)
-print("Your decrypted message is:",enc_msg)
-print("Thank you for using the RSA Decryptor. Goodbye!")
 
-input('Press ENTER to exit') 
+print("\nPesan kamu yang terenkripsi:")
+print(enc_msg, end="\n")
